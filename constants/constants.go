7f2fae14fc203ea4534aefe79040490b0001a0de
kubean-robot <kubean-robot@kubean.io>
2022-09-09 06:03:19 +0000

    See https://github.com/kubean-io/kubean/commit/0bf45d0fed585a3223de8b60c2dddd8f2b810699 from refs/tags/v0.2.2

---
+++ constants/constants.go
@@ -0,0 +1,5 @@
+package constants
+
+const ClusterConfigGlobal = "clusterconfig-global"
+
+const KubeanClusterLabelKey = "clusterName"
---
+++ hack/update-all.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+API_REPO_ROOT=$(pwd) ## /.../kubean/api
+
+bash "$API_REPO_ROOT/hack/update-codegen.sh" kubeancluster
+bash "$API_REPO_ROOT/hack/update-codegen.sh" kubeanclusterops
+bash "$API_REPO_ROOT/hack/update-codegen.sh" kubeanofflineversion
+bash "$API_REPO_ROOT/hack/update-codegen.sh" kubeanclusterconfig
+bash "$API_REPO_ROOT/hack/update-crdgen.sh"
+
+go mod tidy
+go mod vendor
